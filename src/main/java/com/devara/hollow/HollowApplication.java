776cd7d958cdab78c87c29e9ec4c354942dbd9d5
Zaki Devara <mzakidevara@gmail.com>
2026-01-31 21:10:14 +0700

    init springboot

---
+++ src/main/java/com/devara/hollow/HollowApplication.java
@@ -0,0 +1,13 @@
+package com.devara.hollow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HollowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HollowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devara/hollow/HollowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devara.hollow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HollowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
